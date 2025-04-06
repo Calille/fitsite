27f278bdfa2694ee9a91b33135714adef2b4285a
Josh Wicks <josh@keepeducation.com>
2025-04-06 12:27:29 +0100

    Fix metadata exports in page components

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,8 +1,6 @@
-'use client';
-
 import React from 'react';
-import Header from '../../../components/Header';
-import Footer from '../../../components/Footer';
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
 import Image from 'next/image';
 import Link from 'next/link';
 
